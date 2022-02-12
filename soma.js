eb7a1840e59b122b6b9d5f0285c976b29b0411e5
Fábio Araújo <fabiokaia25@gmail.com>
2022-02-12 02:42:29 -0300

    alteração no método soma

--- soma.js
+++ soma.js
@@ -1,4 +1,9 @@
 function soma(a, b) {
-    return a + b
+    if (typeof a === 'string' || typeof b === 'string') {
+        return null;
+    } else {
+        return a + b
+    }
+
 }
 module.exports = soma;
--- soma.test.js
+++ soma.test.js
@@ -4,10 +4,10 @@ test('Soma de 1 + 2 deve ser 3', () => {
   expect(soma(1, 2)).toBe(3);
 });
 
-  it('Deve somar 4 + 4 com resultado 8', () => {
-    expect(soma(4, 4)).toBe(8);
-  });
+it('Deve somar 100 + 1 e o resultado deve ser 101', () => {
+  expect(soma(100, 1)).toBe(101);
+});
 
-  it('Deve somar 200 + 200 e o resultado deve ser 400', () => {
-    expect(soma(4, 4)).toBe(8);
-  });
+it('Não deve somar strings', () => {
+  expect(soma(100, '1')).toBe(null);
+});
